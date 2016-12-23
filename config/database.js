55984cc35145750c548fdcd954ae69bfea66ade6
David Rader <sa@adercon.com>
2016-12-23 06:14:54 -0500

    revert envvars change

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     //url : 'mongodb://db/'
-    //url : 'mongodb://example-mean-todo-db/'
-    url : 'mongodb://'+process.env.APP_DB_HOST+'/' || 'mongodb://db/'
+    url : 'mongodb://example-mean-todo-db/'
+    //url : 'mongodb://'+process.env.APP_DB_HOST+'/' || 'mongodb://db/'
 };
