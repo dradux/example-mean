343419ed3ef742c874924fdfb406f732edf496a4
David Rader <sa@adercon.com>
2016-12-23 13:37:23 -0500

    added db connection error handling

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     //url : 'mongodb://db/'
-    url : 'mongodb://example-mean-todo-db/'
+    url : 'mongodb://example-mean-todo-db-elb/'
     //url : 'mongodb://'+process.env.APP_DB_HOST+'/' || 'mongodb://db/'
 };
