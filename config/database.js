06e95d8cd64f6bfe10d920952095af30e2dd5b2c
David Rader <sa@adercon.com>
2016-12-23 05:55:00 -0500

    changed db to use envvar

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     //url : 'mongodb://db/'
-    url : 'mongodb://example-mean-todo-db-elb/'
+    //url : 'mongodb://example-mean-todo-db/'
+    url : 'mongodb://'+process.env.APP_DB_HOST+'/' || 'mongodb://db/'
 };
